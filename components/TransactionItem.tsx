a34beb2b92354a65f23d54198a31e5fe9e2b6763
Titan Tedha <157805524+TitanTedha@users.noreply.github.com>
2026-06-26 13:27:59 +0700

    Update TransactionItem.tsx

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { formatCurrency } from "app/lib/utils"; // Adjust path if needed
+import { formatCurrency } from "@/lib/utils";
 
 export default function TransactionItem({ tx }: { tx: any }) {
   const router = useRouter();
