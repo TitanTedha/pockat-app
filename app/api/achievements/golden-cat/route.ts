8611503abd8343552b785978f64ae434e4377d3c
Titan Tedha <157805524+TitanTedha@users.noreply.github.com>
2026-06-26 18:32:36 +0700

    Update route.ts

--- app/api/achievements/golden-cat/route.ts
+++ app/api/achievements/golden-cat/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { prisma } from "../../lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/route";
-import { isFirstDayOfMonth, getCurrentMonthAndYear } from "../../../../lib/timeUtils";
+import { isFirstDayOfMonth, getCurrentMonthAndYear } from "../../lib/timeUtils";
 
 export async function POST() {
   const session = await getServerSession(authOptions);
